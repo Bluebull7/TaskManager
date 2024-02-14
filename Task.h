cfb1c5af4833afbf46d663ad742519731bd56f18
Lance H <lsaucyfx@gmail.com>
2024-02-14 18:36:56 -0500

    created task header file

---
+++ Task.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class Task {
+    
+    private:
+    std::string description;
+    bool completed;
+
+    public:
+    Task(const std::string& description);
+    std::string getDescription const;
+    bool isCompleted() const;
+    void markComplete();
+};
