a835c2feeacfa3e85aec0df3ba4d17cf20abad2a
Lance H <lsaucyfx@gmail.com>
2024-02-14 19:43:08 -0500

    updated task.h

--- Task.h
+++ Task.h
@@ -6,10 +6,12 @@ class Task {
     private:
     std::string description;
     bool completed;
+    time_t dueDate; //Due date for the task
 
     public:
-    Task(const std::string& description);
+    Task(const std::string& description, time_t dueDate = 0);
     std::string getDescription() const;
     bool isCompleted() const;
     void markComplete();
+    time_t getDueDate() const; //getter method for due date
 };
