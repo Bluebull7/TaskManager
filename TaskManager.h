53da77af6349ec4feb1c68a6821f0bf4ec9fb870
Lance H <lsaucyfx@gmail.com>
2024-02-14 19:51:21 -0500

    updated Taskmanager.h

--- TaskManager.h
+++ TaskManager.h
@@ -7,7 +7,7 @@ class TaskManager {
         std::vector<Task> tasks;
 
     public:
-        void addTask(const std::string& description);
+        void addTask(const std::string& description, time_t dueDate = 0);
         void markTaskComplete(int index);
         void listTasks() const;
         void saveTasksToFile(const std::string& filename) const;
